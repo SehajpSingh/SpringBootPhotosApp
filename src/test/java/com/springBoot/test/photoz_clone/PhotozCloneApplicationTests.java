7c6aa559b8fb91610cf474b2ee3404ed693dd721
SehajpSingh <sehaj0918@gmail.com>
2024-12-09 09:31:28 -0800

    SpringBoot Application Initilization

---
+++ src/test/java/com/springBoot/test/photoz_clone/PhotozCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springBoot.test.photoz_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
